542251a51a2cfa9c13893076bad07050d167aff4
gida huss <g_hussami@hotmail.com>
2017-11-16 17:48:17 +0100

    a little better wishlist page

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,6 +1,9 @@
 class RetailersController < ApplicationController
   def show
-    @retailer = current_retailer
+    @retailer = Retailer.eager_load(store_collection: [store_products: [product: [:designer]]]).find(current_retailer.id)
+    @store_products_grouped_by_designer = @retailer.store_collection.store_products.group_by do |store_product|
+      store_product.product.designer
+    end
   end
 
   def index
