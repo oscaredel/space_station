89a4d2e58bb324bcce5f072574de6c447dd27fa3
gida huss <g_hussami@hotmail.com>
2017-11-16 15:49:29 +0100

    added map, styled with snazzy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+
   def home
+    @designers = Designer.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@designers) do |designer, marker|
+      marker.lat designer.latitude
+      marker.lng designer.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
   end
+end
 end
