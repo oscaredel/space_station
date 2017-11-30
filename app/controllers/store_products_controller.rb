52d972b5905fcbb594666977632b9508009d72a4
gida huss <g_hussami@hotmail.com>
2017-11-30 14:48:12 +0100

    ajax pt1

--- app/controllers/store_products_controller.rb
+++ app/controllers/store_products_controller.rb
@@ -1,45 +1,18 @@
 class StoreProductsController < ApplicationController
+
   def create
-    if current_retailer
-      @retailer = current_retailer
-      @storecollection = StoreCollection.find_by_retailer_id(@retailer)
-      @product = Product.find(params[:product_id])
-      new_store_product = StoreProduct.new
-      new_store_product.product = @product
-      new_store_product.store_collection = @storecollection
-      if StoreProduct.pluck(:product_id).include?(@product.id)
-        flash[:notice] = "You can't add the same product twice, you dumbo!"
-      else
-        new_store_product.save!
-      end
-      redirect_to designer_path(@product.designer)
-      flash[:notice] = "Succesfully added #{@product.name}"
-    else
-      flash[:notice] = "You are not logged in"
-      redirect_to root_path
-    end
+    @retailer = current_retailer
+    @storecollection = StoreCollection.find_by_retailer_id(@retailer)
+    @product = Product.find(params[:product_id])
+    new_store_product = StoreProduct.new
+    new_store_product.product = @product
+    new_store_product.store_collection = @storecollection
+    new_store_product.save!
   end
 
   def destroy
-   if current_retailer
-      # @retailer = current_retailer
-      # @storecollection = StoreCollection.find_by_retailer_id(@retailer)
-      # @product = Product.find(params[:product_id])
-      # @product.destroy
-      @store_product = StoreProduct.where(product_id: params[:id], store_collection: current_retailer.store_collection).first
-
-      @store_product.destroy
-
-      if session[:previous_page] == "retailers#show"
-        redirect_to retailer_path
-      else
-        redirect_to designer_path(@store_product.product.designer)
-      end
-      flash[:notice] = "Succesfully removed #{@store_product.product.name}"
-    else
-      flash[:notice] = "You are not logged in"
-      redirect_to root_path
-    end
-
+    @store_product = StoreProduct.where(product_id: params[:id], store_collection: current_retailer.store_collection).first
+    @product = @store_product.product
+    @store_product.destroy
   end
 end
