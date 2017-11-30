2947aecb81dc5a47b55204a7e173a037052e0db7
gida huss <g_hussami@hotmail.com>
2017-11-30 15:27:50 +0100

    ajax-star set, city added to designer card

--- app/controllers/store_products_controller.rb
+++ app/controllers/store_products_controller.rb
@@ -8,11 +8,32 @@ class StoreProductsController < ApplicationController
     new_store_product.product = @product
     new_store_product.store_collection = @storecollection
     new_store_product.save!
+
+    respond_to do |format|
+      format.html {
+        redirect_to designer_path(@product.designer)
+        flash[:notice] = "Succesfully added #{@product.name}"
+      }
+      format.js
+    end
   end
 
   def destroy
     @store_product = StoreProduct.where(product_id: params[:id], store_collection: current_retailer.store_collection).first
     @product = @store_product.product
     @store_product.destroy
+
+    respond_to do |format|
+      format.html {
+        if session[:previous_page] == "retailers#show"
+          redirect_to retailer_path
+        else
+          redirect_to designer_path(@store_product.product.designer)
+        end
+        flash[:notice] = "Succesfully removed #{@store_product.product.name}"
+      }
+      format.js
+    end
   end
+
 end
